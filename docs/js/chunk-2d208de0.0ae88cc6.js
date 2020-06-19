538dbeaca95e7a933229747acddd952abd4f9d23
william <william807803@hotmail.com>
2020-06-19 14:18:31 +0800

    build

---
+++ docs/js/chunk-2d208de0.0ae88cc6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d208de0"],{a72f:function(n,e,t){"use strict";t.r(e),t.d(e,"amplify_container",(function(){return r}));var c=t("0170"),i=":host{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}",r=function(){function n(n){Object(c["g"])(this,n)}return n.prototype.render=function(){return Object(c["f"])(c["a"],null,Object(c["f"])("slot",null))},n}();r.style=i}}]);
+//# sourceMappingURL=chunk-2d208de0.0ae88cc6.js.map
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 // vue.config.js
 module.exports = {
+  outputDir: 'docs',
   transpileDependencies: [/\bvue-awesome\b/],
   lintOnSave: false,
 };
